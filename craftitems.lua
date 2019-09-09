398efeadb32f1ac1010d2cedfcddf36ee1677370
test <stub@stub.com>
2019-09-09 12:13:13 +0300

    Added more craft items and recipes for them

--- craftitems.lua
+++ craftitems.lua
@@ -37,3 +37,53 @@ minetest.register_craftitem("magic_materials:enchanted_staff", {
     description = "Blank Enchanted Staff",
     inventory_image = "magic_materials_enchanted_staff.png",
 })
+
+minetest.register_craftitem("magic_materials:magic_root", {
+    description = "Magic Root",
+    inventory_image = "magic_materials_magic_root.png",
+})
+
+minetest.register_craftitem("magic_materials:magic_flower", {
+    description = "Magic Flower",
+    inventory_image = "magic_materials_magic_flower.png",
+})
+
+minetest.register_craftitem("magic_materials:magic_mushroom", {
+    description = "Magic Mushroom",
+    inventory_image = "magic_materials_magic_mushroom.png",
+})
+
+minetest.register_craftitem("magic_materials:fire_rune", {
+    description = "Fire Rune",
+    inventory_image = "magic_materials_fire_rune.png",
+})
+
+minetest.register_craftitem("magic_materials:ice_rune", {
+    description = "Ice Rune",
+    inventory_image = "magic_materials_ice_rune.png",
+})
+
+minetest.register_craftitem("magic_materials:storm_rune", {
+    description = "Storm Rune",
+    inventory_image = "magic_materials_storm_rune.png",
+})
+
+minetest.register_craftitem("magic_materials:void_rune", {
+    description = "Void Rune",
+    inventory_image = "magic_materials_void_rune.png",
+})
+
+minetest.register_craftitem("magic_materials:light_rune", {
+    description = "Light Rune",
+    inventory_image = "magic_materials_light_rune.png",
+})
+
+minetest.register_craftitem("magic_materials:energy_rune", {
+    description = "Energy Rune",
+    inventory_image = "magic_materials_energy_rune.png",
+})
+
+minetest.register_craftitem("magic_materials:earth_rune", {
+    description = "Earth Rune",
+    inventory_image = "magic_materials_earth_rune.png",
+})
--- crafts.lua
+++ crafts.lua
@@ -118,3 +118,103 @@ if minetest.get_modpath("farming") then
         },
     })
 end
+
+minetest.register_craft({
+    type = "shapeless",
+    output = 'magic_materials:magic_root',
+    recipe = {
+            "group:tree",
+            "magic_materials:egerum_crystal",
+    },
+})
+
+minetest.register_craft({
+    type = "shapeless",
+    output = 'magic_materials:magic_flower',
+    recipe = {
+            "group:flower",
+            "magic_materials:egerum_crystal",
+    },
+})
+
+minetest.register_craft({
+    type = "shapeless",
+    output = 'magic_materials:magic_mushroom',
+    recipe = {
+            "flowers:mushroom_red",
+            "magic_materials:egerum_crystal",
+    },
+})
+
+minetest.register_craft({
+    type = "shapeless",
+    output = 'magic_materials:magic_mushroom',
+    recipe = {
+            "flowers:mushroom_brown",
+            "magic_materials:egerum_crystal",
+    },
+})
+
+minetest.register_craft({
+    output = "magic_materials:fire_rune",
+    recipe = {
+        {"default:mese_crystal", "bucket:bucket_lava", "default:mese_crystal"},
+        {"bucket:bucket_lava", "magic_materials:enchanted_rune", "bucket:bucket_lava"},
+        {"default:mese_crystal", "bucket:bucket_lava", "default:mese_crystal"},
+    },
+    replacements = {{ "bucket:bucket_lava", "bucket:bucket_empty"}}
+})
+
+minetest.register_craft({
+    output = "magic_materials:ice_rune",
+    recipe = {
+        {"default:diamond", "default:ice", "default:diamond"},
+        {"default:ice", "magic_materials:enchanted_rune", "default:ice"},
+        {"default:diamond", "default:ice", "default:diamond"},
+    },
+})
+
+minetest.register_craft({
+    output = "magic_materials:storm_rune",
+    recipe = {
+        {"default:diamond", "default:obsidian", "default:diamond"},
+        {"default:obsidian", "magic_materials:enchanted_rune", "default:obsidian"},
+        {"default:diamond", "default:obsidian", "default:diamond"},
+    },
+})
+
+minetest.register_craft({
+    output = "magic_materials:light_rune",
+    recipe = {
+        {"default:mese_crystal", "default:gold_ingot", "default:mese_crystal"},
+        {"default:gold_ingot", "magic_materials:enchanted_rune", "default:gold_ingot"},
+        {"default:mese_crystal", "default:gold_ingot", "default:mese_crystal"},
+    },
+})
+
+minetest.register_craft({
+    output = "magic_materials:void_rune",
+    recipe = {
+        {"magic_materials:arcanite_crystal", "default:obsidian", "magic_materials:arcanite_crystal"},
+        {"default:obsidian", "magic_materials:enchanted_rune", "default:obsidian"},
+        {"magic_materials:arcanite_crystal", "default:obsidian", "magic_materials:arcanite_crystal"},
+    },
+})
+
+minetest.register_craft({
+    output = "magic_materials:energy_rune",
+    recipe = {
+        {"default:diamond", "magic_materials:egerum_crystal", "default:diamond"},
+        {"magic_materials:egerum_crystal", "magic_materials:enchanted_rune", "magic_materials:egerum_crystal"},
+        {"default:diamond", "magic_materials:egerum_crystal", "default:diamond"},
+    },
+})
+
+minetest.register_craft({
+    output = "magic_materials:earth_rune",
+    recipe = {
+        {"default:coal_lump", "default:tin_lump", "default:copper_lump"},
+        {"default:clay_lump", "magic_materials:enchanted_rune", "default:iron_lump"},
+        {"default:gold_lump", "default:mese_crystal", "default:diamond"},
+    },
+})
