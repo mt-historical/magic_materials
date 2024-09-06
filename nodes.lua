fd9f20937ef5ef3e577c961567e9b91d10373bd8
adikalon <webadev@yandex.ru>
2024-09-06 23:53:11 +0300

    Stairs localization

--- nodes.lua
+++ nodes.lua
@@ -39,16 +39,42 @@ minetest.register_node("magic_materials:egerum_block", {
 })
 
 if minetest.get_modpath("stairs") then
-    for k,v in pairs({Arcanite="arcanite", Februm="februm", Egerum="egerum"}) do
-        stairs.register_stair_and_slab(
-            v,
-            "magic_materials:" .. v .. "_block",
-            {cracky=1, level=2},
-            {"magic_materials_" .. v .. "_block.png"},
-            k .. " Stair",
-            k .. " Slab",
-            default.node_sound_stone_defaults(),
-            false
-        )
-    end
+    stairs.register_stair_and_slab(
+        "arcanite",
+        "magic_materials:arcanite_block",
+        {cracky=1, level=2},
+        {"magic_materials_arcanite_block.png"},
+        magic_materials.S("Arcanite Stair"),
+        magic_materials.S("Arcanite Slab"),
+        default.node_sound_stone_defaults(),
+        false,
+        magic_materials.S("Inner Arcanite Stair"),
+        magic_materials.S("Outer Arcanite Stair")
+    )
+
+    stairs.register_stair_and_slab(
+        "februm",
+        "magic_materials:februm_block",
+        {cracky=1, level=2},
+        {"magic_materials_februm_block.png"},
+        magic_materials.S("Februm Stair"),
+        magic_materials.S("Februm Slab"),
+        default.node_sound_stone_defaults(),
+        false,
+        magic_materials.S("Inner Februm Stair"),
+        magic_materials.S("Outer Februm Stair")
+    )
+
+    stairs.register_stair_and_slab(
+        "egerum",
+        "magic_materials:egerum_block",
+        {cracky=1, level=2},
+        {"magic_materials_egerum_block.png"},
+        magic_materials.S("Egerum Stair"),
+        magic_materials.S("Egerum Slab"),
+        default.node_sound_stone_defaults(),
+        false,
+        magic_materials.S("Inner Egerum Stair"),
+        magic_materials.S("Outer Egerum Stair")
+    )
 end
